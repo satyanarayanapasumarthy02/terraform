e5d3db6f4bc24bd92b57f4b8c43b83dc1e08200b
Satyanarayana P <satyanarayana.pasumarthy02@gmail.com>
2025-10-15 18:11:32 +0530

    terraform

---
+++ for-loop/variables.tf
@@ -0,0 +1,24 @@
+variable "instances" {
+    default = [ "mongodb", "redis"]
+    # default = {
+    #     mongodb = "t3.micro"
+    #     redis = "t3.micro"
+    #     mysql = "t3.small"
+    # }
+    # default = {
+    #     mongodb = {
+    #         instance_type = "t3.micro"
+    #         ami = "ami-id"
+    #     }
+    #     redis = "t3.micro"
+    #     mysql = "t3.small"
+    # }
+}
+
+variable "zone_id" {
+    default = "Z04238403T15D40S55XVR"
+}
+
+variable "domain_name" {
+    default = "cloudtechrnd.space"
+}
